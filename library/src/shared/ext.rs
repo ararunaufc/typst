1fc774e1576f6bdc348fc078f41c38d28cae4030
Arthur Araruna <ararunaufc@gmail.com>
2023-04-10 10:30:53 -0300

    Add fail-fast iterator trait ext and impl.
    This allow us to bail early when we iterate through `Result`s and want
    to early-return upon `Err`.

--- library/src/shared/ext.rs
+++ library/src/shared/ext.rs
@@ -1,5 +1,7 @@
 //! Extension traits.
 
+use std::marker::PhantomData;
+
 use crate::layout::{AlignElem, MoveElem, PadElem};
 use crate::prelude::*;
 use crate::text::{EmphElem, FontFamily, FontList, StrongElem, TextElem, UnderlineElem};
@@ -77,5 +79,92 @@ impl StylesExt for Styles {
                 .chain(TextElem::font_in(existing))
                 .collect(),
         )));
+    }
+}
+/// [`Iterator`] extension trait for fail-fast operations.
+pub trait FailFastIteratorExt: Iterator {
+    /// Maps [`Iterator`] values with a custom function when the [`Iterator::Item`]s
+    /// are [`Result`]s.
+    ///
+    /// Upon the first time that the original `Iterator` yields a [`Result::Err`],
+    /// the `Iterator` resulting from this function will yield that value and
+    /// then refuse to yield another value.
+    fn try_map_ok<T, E, F, U>(self, mapfn: F) -> FailFastMapIter<Self, T, E, F>
+    where
+        Self: Iterator<Item = Result<T, E>> + Sized,
+        F: FnMut(T) -> U,
+    {
+        FailFastMapIter {
+            iter: self,
+            bail: false,
+            mapfn,
+            _phantom: PhantomData,
+        }
+    }
+
+    /// Folds [`Iterator`]s values with a custom function when the [`Iterator::Item`]s
+    /// are [`Result`]s.
+    ///
+    /// This function operates only on the [`Result::Ok`] values yielded by the
+    /// iterator. Upon the first time that the original `Iterator` yields a
+    /// [`Result::Err`], this function will return that value and stop processing
+    /// the remaining items.
+    fn try_fold_ok<T, E, B, F>(mut self, initial: B, mut foldfn: F) -> Result<B, E>
+    where
+        Self: Iterator<Item = Result<T, E>> + Sized,
+        F: FnMut(B, T) -> B,
+    {
+        let mut folded = initial;
+
+        loop {
+            match self.next() {
+                Some(Ok(value)) => {
+                    folded = foldfn(folded, value);
+                }
+                Some(Err(error)) => return Err(error),
+                None => return Ok(folded),
+            }
+        }
+    }
+}
+
+impl<I, T, E> FailFastIteratorExt for I where I: Iterator<Item = Result<T, E>> {}
+
+/// An [`Iterator`] that will short-circuit some of its operations.
+///
+/// This Iterator's accepts [`Result`]s as its [`Item`][`Iterator::Item`]s and,
+/// when that yields a [`Result::Err`] value for the first time, it is guaranteed
+/// that this will never yield another item.
+pub struct FailFastMapIter<I, T, E, F> {
+    bail: bool,
+    iter: I,
+    mapfn: F,
+    // ? QUESTION: Is this correct from variance perspective?
+    _phantom: PhantomData<(T, E)>,
+}
+
+impl<I, T, E, F, U> Iterator for FailFastMapIter<I, T, E, F>
+where
+    I: Iterator<Item = Result<T, E>>,
+    F: FnMut(T) -> U,
+{
+    type Item = Result<U, E>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.bail {
+            None
+        } else {
+            match self.iter.next() {
+                Some(Ok(value)) => Some(Ok((self.mapfn)(value))),
+                Some(Err(error)) => {
+                    self.bail = true;
+                    Some(Err(error))
+                }
+                None => {
+                    self.bail = true;
+                    None
+                }
+            }
+        }
     }
 }
